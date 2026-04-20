LineageOS/android_kernel_qcom_sm8750

DNM: build: bazel: Update hardcoded paths
TODO: Find a way to make these dynamic
Change-Id: I84f23cdbacf12f3315ded6fa7d4bc0930a88fe04

msm_kernel_16k_la.bzl
@@ -46,7 +46,7 @@ def _define_build_config(
 
     gen_config_command = """
       cat << 'EOF' > "$@"
-KERNEL_DIR="msm-kernel"
+KERNEL_DIR="vendor/qcom/kernel"
 VARIANTS=(%s)
 MSM_ARCH=%s
 VARIANT=%s

msm_kernel_extensions.bzl
@@ -1,5 +1,5 @@
 load("@bazel_skylib//rules:common_settings.bzl", "bool_flag")
-load("//msm-kernel/arch/arm64/boot/dts/vendor:qcom/platform_map.bzl", _get_dtb_list = "get_dtb_list", _get_dtbo_list = "get_dtbo_list")
+load("//vendor/qcom/kernel/arch/arm64/boot/dts/vendor:qcom/platform_map.bzl", _get_dtb_list = "get_dtb_list", _get_dtbo_list = "get_dtbo_list")
 
 def define_top_level_rules():
     for skippable in ["abl", "dtc", "abi"]:
@@ -31,7 +31,7 @@ def get_dtbo_list(target):
     return _get_dtbo_list(target)
 
 def get_dtstree(target):
-    return "//msm-kernel/arch/arm64/boot/dts/vendor:msm_dt"
+    return "//vendor/qcom/kernel/arch/arm64/boot/dts/vendor:msm_dt"
 
 def get_vendor_ramdisk_binaries(target, flavor = None):
     return None

msm_kernel_la.bzl
@@ -48,7 +48,7 @@ def _define_build_config(
 
     gen_config_command = """
       cat << 'EOF' > "$@"
-KERNEL_DIR="msm-kernel"
+KERNEL_DIR="vendor/qcom/kernel"
 VARIANTS=(%s)
 MSM_ARCH=%s
 VARIANT=%s

msm_kernel_le.bzl
@@ -48,7 +48,7 @@ def _define_build_config(
     earlycon_param = "earlycon" + earlycon_param
     gen_config_command = """
       cat << 'EOF' > "$@"
-KERNEL_DIR="msm-kernel"
+KERNEL_DIR="vendor/qcom/kernel"
 VARIANTS=(%s)
 MSM_ARCH=%s
 VARIANT=%s

msm_kernel_vm.bzl
@@ -66,7 +66,7 @@ def _define_build_config(
 
     gen_config_command = """
       cat << 'EOF' > "$@"
-KERNEL_DIR="msm-kernel"
+KERNEL_DIR="vendor/qcom/kernel"
 VARIANTS=(%s)
 MSM_ARCH=%s
 VARIANT=%s

LineageOS/android_kernel_ayn_cq8725s

msm_kernel_16k_la.bzl
@@ -46,7 +46,7 @@ def _define_build_config(
 
     gen_config_command = """
       cat << 'EOF' > "$@"
-KERNEL_DIR="msm-kernel"
+KERNEL_DIR="vendor/ayn/kernel"
 VARIANTS=(%s)
 MSM_ARCH=%s
 VARIANT=%s

msm_kernel_extensions.bzl
@@ -1,5 +1,5 @@
 load("@bazel_skylib//rules:common_settings.bzl", "bool_flag")
-load("//msm-kernel/arch/arm64/boot/dts/vendor:qcom/platform_map.bzl", _get_dtb_list = "get_dtb_list", _get_dtbo_list = "get_dtbo_list")
+load("//vendor/ayn/kernel/arch/arm64/boot/dts/vendor:qcom/platform_map.bzl", _get_dtb_list = "get_dtb_list", _get_dtbo_list = "get_dtbo_list")
 
 def define_top_level_rules():
     for skippable in ["abl", "dtc", "abi"]:
@@ -31,7 +31,7 @@ def get_dtbo_list(target):
     return _get_dtbo_list(target)
 
 def get_dtstree(target):
-    return "//msm-kernel/arch/arm64/boot/dts/vendor:msm_dt"
+    return "//vendor/ayn/kernel/arch/arm64/boot/dts/vendor:msm_dt"
 
 def get_vendor_ramdisk_binaries(target, flavor = None):
     return None

msm_kernel_la.bzl
@@ -48,7 +48,7 @@ def _define_build_config(
 
     gen_config_command = """
       cat << 'EOF' > "$@"
-KERNEL_DIR="msm-kernel"
+KERNEL_DIR="vendor/ayn/kernel"
 VARIANTS=(%s)
 MSM_ARCH=%s
 VARIANT=%s

msm_kernel_le.bzl
@@ -48,7 +48,7 @@ def _define_build_config(
     earlycon_param = "earlycon" + earlycon_param
     gen_config_command = """
       cat << 'EOF' > "$@"
-KERNEL_DIR="msm-kernel"
+KERNEL_DIR="vendor/ayn/kernel"
 VARIANTS=(%s)
 MSM_ARCH=%s
 VARIANT=%s

msm_kernel_vm.bzl
@@ -66,7 +66,7 @@ def _define_build_config(
 
     gen_config_command = """
       cat << 'EOF' > "$@"
-KERNEL_DIR="msm-kernel"
+KERNEL_DIR="vendor/ayn/kernel"
 VARIANTS=(%s)
 MSM_ARCH=%s
 VARIANT=%s